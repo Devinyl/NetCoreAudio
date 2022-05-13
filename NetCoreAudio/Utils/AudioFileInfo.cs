0d7ef7829cbb5e3505561b631aa6730c8cc81222
Hrkrx <5176531+hrkrx@users.noreply.github.com>
2022-05-13 06:27:05 +0200

    AudioFileInfo

---
+++ NetCoreAudio/Utils/AudioFileInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetCoreAudio.Utils
+{
+    public  class AudioFileInfo
+    {
+        public int Length { get; set; }
+        public string FileName { get; set; }
+        public string FilePath { get; set; }
+        public string FileExtension { get; set; }
+        public string Artist { get; set; }
+        public string Title { get; set; }
+        public string Album { get; set; }
+        public string Genre { get; set; }
+        public string Year { get; set; }
+        public string Track { get; set; }
+        public string Comment { get; set; }
+        public long FileSize { get; internal set; }
+    }
+}
